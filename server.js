13a7c3b8f9a3e4354ecb464a5cb86845557ebe73
AntonhyBPV <antonio.benja.parra.velasquez@gmail.com>
2026-04-07 20:35:28 -0500

    index file

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
 app.use(helmet());
 app.use(express.json());
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-  res.send('Servidor TLS funcionando - Deploy automatico: OK');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
